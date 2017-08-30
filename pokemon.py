4490ad7a131abe325e268c52ecf447da88b89c2f
Peter O'Brien <pjo2412@gmail.com>
2017-08-29 23:34:46 -0400

    Added ZoneManager class

--- pokemon.py
+++ pokemon.py
@@ -1,48 +1,40 @@
 from math import sin, cos, sqrt, atan2, radians
 from datetime import datetime
-from orm.models import Sighting
+from orm.models import Sighting, PokemonZone
+
 
 class PokemonManager:
     def __init__(self):
-        self.monsters = []
+        self.active_sightings = []
 
     def create_pokemon(self, pokemon_name, pokemon_number, end, latitude, longitude):
-        sighting = Sighting(pokemon_name=pokemon_name, pokemon_number=pokemon_number, expiration=end, latitude=latitude, longitude=longitude)
+        sighting = Sighting(pokemon_name=pokemon_name, pokemon_number=pokemon_number, expiration=end, latitude=latitude,
+                            longitude=longitude)
         sighting.save()
-        self.monsters.append(sighting)
+        self.active_sightings.append(sighting)
         return sighting
 
     def remove_pokemon(self, sighting):
-        self.monsters.remove(sighting)
+        self.active_sightings.remove(sighting)
 
-class PokemonZone:
-    def __init__(self,channel,lat,lon,radius):
-        self.channel = channel
-        self.latitude = float(lat)
-        self.longitude = float(lon)
-        self.radius = float(radius)
-        self.targetPokemon = []
 
-    def isInZone(self, sighting):
-        earth_radius = 6373.0
+class ZoneManager:
+    def __init__(self):
+        self.zones = dict()
 
-        center_lat = radians(self.latitude)
-        center_lon = radians(self.longitude)
-        gym_lat = radians(sighting.latitude)
-        gym_lon = radians(sighting.longitude)
+    def create_zone(self, destination, latitude, longitude):
+        pz = PokemonZone(destination=destination, latitude=latitude, longitude=longitude)
+        pz.save()
+        self.zones[destination] = pz
+        return pz
 
-        dlon = gym_lon - center_lon
-        dlat = gym_lat - center_lat
-
-        a = sin(dlat / 2)**2 + cos(center_lat) * cos(gym_lat) * sin(dlon / 2)**2
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-
-        distance = earth_radius * c
-
-        return distance <= self.radius
-
-    def filterPokemon(self, pokemonNumber):
-        if len(self.targetPokemon) == 0:
-            return True
-        else:
-            return int(pokemonNumber) in self.targetPokemon
+    async def load_from_database(self, discordServer):
+        for pz in PokemonZone.objects.all():
+            channel = discordServer.get_channel(pz.destination)
+            if channel is None:
+                channel = discordServer.get_member(pz.destination)
+            if channel is not None:
+                pz.discord_destination = channel
+                self.zones[pz.destination] = pz
+            else:
+                print('Unable to load pokemon zone for id {} destination {}'.format(pz.id, pz.destination))
