44351adcdd515993eee267ba2a65dba61ffa52e8
Peter O'Brien <pjo2412@gmail.com>
2017-08-29 22:01:20 -0400

    Fixed defect if DATABASE or SECURITY could not be found.

--- setup.py
+++ setup.py
@@ -10,10 +10,14 @@ for key in defaultValues.keys():
     if key not in config['DEFAULT']:
         config['DEFAULT'][key] = defaultValues[key]
 
+if 'DATABASES' not in config:
+    config['DATABASES'] = dict()
 for key in databaseValues.keys():
     if key not in config['DATABASES']:
         config['DATABASES'][key] = databaseValues[key]
 
+if 'SECURITY' not in config:
+    config['SECURITY'] = dict()
 for key in securityValues.keys():
     if key not in config['SECURITY']:
         config['SECURITY'][key] = securityValues[key]
